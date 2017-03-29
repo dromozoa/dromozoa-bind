d2ecd4ebb0c177e028cd734edbd6a45e362d2d8c
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-03-29 09:15:42 +0900

    テストを修正。

--- test/test.lua
+++ test/test.lua
@@ -228,6 +228,35 @@ local result, message = pcall(bind.get_field_without_state)
 print(result, message)
 assert(not result)
 
+local thread = coroutine.create(function ()
+  local thread = coroutine.create(function ()
+    local thread = coroutine.create(function ()
+      local thread = coroutine.create(function ()
+        bind.set_callback(print, print)
+        collectgarbage()
+        collectgarbage()
+      end)
+      assert(coroutine.resume(thread))
+      thread = nil
+      collectgarbage()
+      collectgarbage()
+    end)
+    assert(coroutine.resume(thread))
+    thread = nil
+    collectgarbage()
+    collectgarbage()
+  end)
+  assert(coroutine.resume(thread))
+  thread = nil
+  collectgarbage()
+  collectgarbage()
+end)
+assert(coroutine.resume(thread))
+thread = nil
+collectgarbage()
+collectgarbage()
 
-
-
+bind.run_callback_s("foo")
+bind.run_callback_s("bar")
+bind.run_callback_s("baz")
+bind.run_destructor()
